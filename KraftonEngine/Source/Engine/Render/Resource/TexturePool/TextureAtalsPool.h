31f97bdfeff195b594175c265e62604f2aeed125
Park SangHyeok <tkdgurehf00@gmail.com>
2026-04-26 19:46:27 +0900

    feat : FTexturePool 추가

---
+++ KraftonEngine/Source/Engine/Render/Resource/TexturePool/TextureAtalsPool.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "TexturePool.h"
+#include "UVManager/FUVManager.h"
+
+
+
+class FTextureAtlasPool final : public FTexturePoolBase
+{
+	template<typename T>
+	using TComPtr = Microsoft::WRL::ComPtr<T>;
+
+public:
+	bool GetTextureHandle(float TextureSize, TexturePoolHandle& OutHandle) override;;
+	void ReleaseHandle(TexturePoolHandle& InHandle) override;
+	AtlasUV GetAtlasUV(const TexturePoolHandle& InHandle) { return UVManagers[InHandle.ArrayIndex].get()->GetAtlasUV(InHandle.InternalIndex); }
+
+	ID3D11DepthStencilView* GetDSV(TexturePoolHandle* InHandle) { return DSVs[InHandle->ArrayIndex].Get(); }
+	ID3D11ShaderResourceView* GetDebugSRV(const TexturePoolHandle& InHandle) override;
+
+protected:
+	virtual TComPtr<ID3D11Texture2D> CreateTexture(ID3D11Device* Device) override;
+	virtual void RebuildSRV(ID3D11Device* Device, ID3D11Texture2D* InTexture) override;
+
+	virtual void BroadCastEntries() override;
+
+	virtual void OnSetTextureSize() override;
+	virtual void OnSetTextureLayerSize() override;
+
+private:
+	TArray<std::unique_ptr<FUVManagerBase>> UVManagers;
+};
+
---
+++ KraftonEngine/Source/Engine/Render/Resource/TexturePool/TexturePool.h
@@ -0,0 +1,86 @@
+﻿#pragma once
+#include "Core/CoreTypes.h"
+#include "Core/Singleton.h"
+#include "d3d11.h"
+#include <wrl/client.h>
+#include <memory>
+
+
+class FTexturePoolBase : public TSingleton<FTexturePoolBase>
+{
+	template<typename T>
+	using TComPtr = Microsoft::WRL::ComPtr<T>;
+
+#pragma region InternalStruct
+public:
+	struct TexturePoolHandle
+	{
+		TexturePoolHandle(uint32 InInternalIndex, uint32 InArrayIndex)
+			: InternalIndex(InInternalIndex), ArrayIndex(InArrayIndex) {
+		}
+
+		float GetIndex() { return InternalIndex; }
+		void Release() { Pool->ReleaseHandle(*this); }
+
+		bool operator==(const TexturePoolHandle& Other) const
+		{
+			return InternalIndex == Other.InternalIndex &&
+				ArrayIndex == Other.ArrayIndex;
+		}
+
+		uint32 InternalIndex = -1;
+		uint32 ArrayIndex = -1;
+
+	private:
+		FTexturePoolBase* Pool = nullptr;
+	};
+
+private:
+	struct SRVResource
+	{
+		TComPtr<ID3D11Texture2D> Texture;
+		TComPtr<ID3D11ShaderResourceView> SRV;
+	};
+#pragma endregion
+
+public:
+	void Initialize(ID3D11Device* InDevice, ID3D11DeviceContext* InDeviceContext, uint32 InTextureSize);
+
+	virtual bool GetTextureHandle(float TextureSize, TexturePoolHandle& OutHandle) { return false; }
+	virtual void ReleaseHandle(TexturePoolHandle& InHandle) {};
+	virtual ID3D11ShaderResourceView* GetDebugSRV(const TexturePoolHandle& InHandle) { return nullptr; }
+
+protected:
+	virtual TComPtr<ID3D11Texture2D> CreateTexture(ID3D11Device* Device) { return nullptr; }
+	virtual void RebuildSRV(ID3D11Device* Device, ID3D11Texture2D* InTexture) {};
+	void RebuildDSV(ID3D11Device* Device, ID3D11Texture2D* InTexture);
+
+	void ResizeLayer() { ResizeLayer(TextureLayerSize * 2); }
+	void ResizeLayer(uint32 InNewLayerSize);
+
+	virtual void BroadCastEntries() {};
+
+	virtual void OnSetTextureSize() {};
+	virtual void OnSetTextureLayerSize() {};
+
+	virtual uint32 GetTextureLayerSize() { return TextureLayerSize; }
+
+private:
+	void SetTextureLayerSize(uint32 InTextureLayerSize);
+	void SetTextureSize(uint32 InTextureSize);
+
+protected:
+	uint32 TextureLayerSize = 1;
+	uint32 TextureSize = -1;
+
+	TComPtr<ID3D11ShaderResourceView> SRV;
+	TArray<TComPtr<ID3D11DepthStencilView>> DSVs;
+
+	TMap<float, std::unique_ptr<SRVResource>> DebugResource; //ImGui::Image에 렌더링할 대표 이미지, Size
+
+private:
+	TComPtr<ID3D11Texture2D> Texture;
+
+	ID3D11Device* Device = nullptr;
+	ID3D11DeviceContext* DeviceContext = nullptr;
+};
