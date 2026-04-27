57f988acc0e8853e6a4868a0ded4c6ac69c51368
Park SangHyeok <tkdgurehf00@gmail.com>
2026-04-27 14:58:07 +0900

    1000번째 커밋

---
+++ KraftonEngine/Source/Engine/Render/Resource/TexturePool/CubeMapPool.cpp
@@ -0,0 +1,48 @@
+﻿//#include "CubeMapPool.h"
+//
+//template<typename T>
+//using TComPtr = Microsoft::WRL::ComPtr<T>;
+//using TexturePoolHandle = FTexturePoolBase::TexturePoolHandle;
+//using TexturePoolHandleSet = FTexturePoolBase::TexturePoolHandleSet;
+//
+//TexturePoolHandleSet* FCubeMapPool::GetTextureHandle(TexturePoolHandleRequest HandleRequest)
+//{
+//	return nullptr;
+//}
+//
+//void FCubeMapPool::ReleaseHandle(TexturePoolHandle& InHandle)
+//{
+//}
+//
+//TComPtr<ID3D11Texture2D> FCubeMapPool::CreateTexture(ID3D11Device* Device)
+//{
+//	D3D11_TEXTURE2D_DESC desc = {};
+//	desc.Width = TextureSize;
+//	desc.Height = TextureSize;
+//	desc.MipLevels = 1;
+//	desc.ArraySize = TextureLayerSize * 6;
+//	desc.Format = DXGI_FORMAT_R32_TYPELESS;
+//	desc.SampleDesc.Count = 1;
+//	desc.Usage = D3D11_USAGE_DEFAULT;
+//	desc.MiscFlags = 0;
+//	desc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_DEPTH_STENCIL;
+//	desc.CPUAccessFlags = 0;
+//
+//	TComPtr<ID3D11Texture2D> NewTexture;
+//	HRESULT hr = Device->CreateTexture2D(&desc, nullptr, NewTexture.GetAddressOf());
+//	assert(SUCCEEDED(hr));
+//
+//	return NewTexture;
+//}
+//
+//void FCubeMapPool::RebuildSRV(ID3D11Device* Device, ID3D11Texture2D* InTexture)
+//{
+//}
+//
+//void FCubeMapPool::OnSetTextureSize()
+//{
+//}
+//
+//void FCubeMapPool::OnSetTextureLayerSize()
+//{
+//}
